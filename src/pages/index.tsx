0a565c3a6360228075cf21ef8b4e83bd2729506c
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2021-02-24 17:09:09 -0300

    ChallendeBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallegens } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -22,9 +23,9 @@ export default function Home() {
         <CompletedChallegens />
         <Countdown />
       </div>
-
-
-      <div></div>
+      <div>
+        <ChallengeBox />
+      </div>
     </section>
 
 
